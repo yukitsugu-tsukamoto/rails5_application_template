831d5f1aa9945b20ff7ff8f2a933e12633937794
yukitsugu tsukamoto <yukitsugu.tsukamoto@gmail.com>
2017-03-01 18:01:34 +0900

    remove wercker

--- app_template.rb
+++ app_template.rb
@@ -315,11 +315,11 @@ end
 
 # Wercker(CI)
 # ----------------------------------------------------------------
-if yes?('Do you use wercker? [yes or ELSE]')
-  run 'wget https://raw.githubusercontent.com/morizyun/rails5_application_template/master/root/wercker.yml'
-  gsub_file 'wercker.yml', /%RUBY_VERSION/, ruby_version
-  run "echo 'Please Set SLACK_URL to https://app.wercker.com'"
-end
+# if yes?('Do you use wercker? [yes or ELSE]')
+#   run 'wget https://raw.githubusercontent.com/morizyun/rails5_application_template/master/root/wercker.yml'
+#   gsub_file 'wercker.yml', /%RUBY_VERSION/, ruby_version
+#   run "echo 'Please Set SLACK_URL to https://app.wercker.com'"
+# end
 
 # Rubocop Auto correct
 # ----------------------------------------------------------------
